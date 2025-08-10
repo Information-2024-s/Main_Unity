e2a5caa630e6b43b9ea68b03f5563c12fbcb7f00
yakinikusan-cmd <hkhs0106@gmail.com>
2025-08-10 20:40:54 +0900

    ランダムで速度変わるようにしたけどちょいびみょい

--- Assets/scripts/Moving_1.cs
+++ Assets/scripts/Moving_1.cs
@@ -3,7 +3,9 @@ using UnityEngine;
 public class MoveSideToSide : MonoBehaviour
 {
     public float speed = 2.0f; // 移動速度
-    public Vector3 distance = new Vector3(3.0f ,0.0f , 0.0f); // 左右前後上下の移動距離
+    public Vector3 distance = new Vector3(3.0f, 0.0f, 0.0f); // 左右前後上下の移動距離
+    public bool random_enable = false;
+    public float random_range = 0.5f;
     private Vector3 startPosition;
     private bool movingForward = true;
 
@@ -18,13 +20,15 @@ public class MoveSideToSide : MonoBehaviour
         float targetX = movingForward ? startPosition.x + distance.x : startPosition.x - distance.x;
         float targetY = movingForward ? startPosition.y + distance.y : startPosition.y - distance.y;
         float targetZ = movingForward ? startPosition.z + distance.z : startPosition.z - distance.z;
-        
+
         // 位置を補間
-        transform.position = Vector3.Lerp(transform.position, new Vector3(targetX, targetY, targetZ), speed * Time.deltaTime);
+        transform.position = Vector3.Lerp(transform.position, new Vector3(targetX, targetY, targetZ), (random_enable?Random.Range(Mathf.Min(random_range,1.0f),Mathf.Max(random_range,1.0f)):1.0f)*speed * Time.deltaTime);
+        Debug.Log($"Lerp t: {(random_enable?Random.Range(Mathf.Min(random_range,1.0f),Mathf.Max(random_range,1.0f)):1.0f)*speed * Time.deltaTime}");
         // 目的地に到達したら反転
-        if (Mathf.Abs(transform.position.x - targetX) +  Mathf.Abs(transform.position.y - targetY) + Mathf.Abs(transform.position.z - targetZ)< 0.1f)
+        if (Mathf.Abs(transform.position.x - targetX) + Mathf.Abs(transform.position.y - targetY) + Mathf.Abs(transform.position.z - targetZ) < 0.1f)
         {
             movingForward = !movingForward;
         }
     }
+
 }
