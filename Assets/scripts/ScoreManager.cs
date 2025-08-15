8c912023a79c3b9e1bd60f1a3751d6b00d9090c8
yakinikusan-cmd <hkhs0106@gmail.com>
2025-08-15 20:08:02 +0900

    DBサーバーへのスコアの送信を実装(未実験)

--- Assets/scripts/ScoreManager.cs
+++ Assets/scripts/ScoreManager.cs
@@ -1,5 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.Networking;
 
 public class ScoreManager : MonoBehaviour
 {
@@ -14,14 +21,25 @@ public class ScoreManager : MonoBehaviour
             instance = this;
         }
     }
+    public class ScoreJson {
+        public int player_id;
+        public int score;
+    }
 
-    public void AddScore(int player,int amount)
+    public void AddScore(int player, int amount)
     {
         Debug.Log(player);
         scores[player] += amount;
         UpdateScoreUI(player);
     }
-
+    public void send_score(int player_id,int score)
+    {
+        ScoreJson ScoreData = new ScoreJson();
+        ScoreData.player_id = player_id;
+        ScoreData.score = score;
+        string jsonstr = JsonUtility.ToJson (ScoreData);
+        StartCoroutine(Post("http://localhost:3000/api/scores", jsonstr));
+    }
     private void UpdateScoreUI(int player)
     {
         if (scoreText != null)
@@ -29,6 +47,18 @@ public class ScoreManager : MonoBehaviour
             Debug.Log(player);
             scoreText[player].text = "" + scores[player];
         }
+    }
+    IEnumerator Post(string url, string jsonstr)
+    {
+        var request = new UnityWebRequest(url, "POST");
+        byte[] bodyRaw = Encoding.UTF8.GetBytes(jsonstr);
+        request.uploadHandler = (UploadHandler) new UploadHandlerRaw(bodyRaw);
+        request.downloadHandler = (DownloadHandler) new DownloadHandlerBuffer();
+        request.SetRequestHeader("Content-Type", "application/json");
+
+        yield return request.SendWebRequest();
+
+        Debug.Log("Status Code: " + request.responseCode);
     }
 
 }
