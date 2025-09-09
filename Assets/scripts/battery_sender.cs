d9d3e2667b225450789202dfdeb7b66ad4984c02
yakinikusan-cmd <hkhs0106@gmail.com>
2025-09-10 02:08:19 +0900

    軽微な修正

--- Assets/scripts/battery_sender.cs
+++ Assets/scripts/battery_sender.cs
@@ -4,7 +4,7 @@ using UnityEngine.Networking;
 using System.Text;
 
 
-public class log_sender : MonoBehaviour
+public class battery_sender : MonoBehaviour
 {
 
     public class BatteryJson
@@ -13,19 +13,19 @@ public class log_sender : MonoBehaviour
         public int controller_num;
         public int battery_level;
     }   
-    public void send_battery_level{
+    public void send_battery_level()
+    {
         BatteryJson battery_data = new BatteryJson();
         battery_data.stage = config_loader.config.stage;
         
     }
     IEnumerator Post(string url, string jsonstr)
     {
-        var request = new UnityWebRequest(url, "PUT");
-        byte[] bodyRaw = Encoding.UTF8.GetBytes(jsonstr);
-        request.uploadHandler = (UploadHandler) new UploadHandlerRaw(bodyRaw);
-        request.downloadHandler = (DownloadHandler) new DownloadHandlerBuffer();
-        request.SetRequestHeader("Content-Type", "application/json");
-
-        yield return request.SendWebRequest();
+    var request = new UnityWebRequest(url, "PUT");
+    byte[] bodyRaw = Encoding.UTF8.GetBytes(jsonstr);
+    request.uploadHandler = (UploadHandler)new UploadHandlerRaw(bodyRaw);
+    request.downloadHandler = (DownloadHandler)new DownloadHandlerBuffer();
+    request.SetRequestHeader("Content-Type", "application/json");
+    yield return request.SendWebRequest();
     }
 }
