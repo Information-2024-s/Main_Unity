6400e6dc218a81248193b7d9ba1c988d3fe154f3
yakinikusan-cmd <hkhs0106@gmail.com>
2025-10-19 15:58:23 +0900

    文字も追加

--- Assets/scripts/title_video_controller.cs
+++ Assets/scripts/title_video_controller.cs
@@ -14,6 +14,9 @@ public class TitleVideoController : MonoBehaviour
 
     private VideoPlayer videoPlayer;
     public GameObject loop_video;
+    public GameObject logo_object;
+    private Image logo_image; 
+
 
     void Start()
     {
@@ -25,7 +28,7 @@ public class TitleVideoController : MonoBehaviour
             return;
         }
         loop_video.SetActive(true);
-
+        logo_image = logo_object.GetComponent<Image>();
 
 
     }
@@ -39,12 +42,31 @@ public class TitleVideoController : MonoBehaviour
 
     IEnumerator transion_next_scene()
     {
-        
+
         videoPlayer.Play();
+        StartCoroutine(FadeImage(logo_image,1));
         yield return new WaitUntil(() => videoPlayer.isPlaying);
         loop_video.SetActive(false);
         yield return new WaitWhile(() => videoPlayer.isPlaying);
         SceneManager.LoadScene("QR_read");
+    }
+        private IEnumerator FadeImage(Image img, float duration)
+    {
+        Color color = img.color;
+        float startAlpha = color.a;
+        float time = 0f;
+
+        while (time < duration)
+        {
+            time += Time.deltaTime;
+            color.a = Mathf.Lerp(startAlpha, 0f, time / duration);
+            img.color = color;
+            yield return null;
+        }
+
+        color.a = 0f;
+        img.color = color;
+        img.gameObject.SetActive(false); // 完全に非表示にする場合
     }
 
 }
